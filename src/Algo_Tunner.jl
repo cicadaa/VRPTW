2642811add7e28b73954345c30e7217ad838ca0b
cicadaaa <cicaday@outlook.com>
2020-04-29 19:38:34 +0200

    cleaned visualisation part

--- src/Algo_Tunner.jl
+++ src/Algo_Tunner.jl
@@ -4,10 +4,10 @@ using AlgoTuner
 include("ALNS_Solver.jl")
 
 function getBestKnown()
-    instanceSet = ["C1_2_1.TXT","C2_2_7.TXT","R1_2_7.TXT","R2_2_5.TXT"]
+    instanceSet = ["C1_2_1.TXT","C2_2_7.TXT","R1_2_7.TXT"]#,"R2_2_5.TXT"]
     bestKnown = Dict()
     for inst in instanceSet
-        bestKnown[inst] = alns_solver(123, inst, 10, 1, 3, 0.004, 20, 1)
+        bestKnown[inst] = alns_solver(123, inst, 30, 1, 3, 0.005, 20, 1)
     end
     return instanceSet, bestKnown
 end
@@ -16,18 +16,18 @@ end
 benchMark, bestKnown = getBestKnown()
 
 tunnerALNS(seed, instance, d_ran_routes, d_ratio, d_knn, d_exp_routes)=
-(alns_solver(seed, instance, 10, 1, d_ran_routes, d_ratio, d_knn, d_exp_routes)
+(alns_solver(seed, instance, 30, 1, d_ran_routes, d_ratio, d_knn, d_exp_routes)
 - bestKnown[instance])/bestKnown[instance]
 
 
 cmd = AlgoTuner.createRuntimeCommand(tunnerALNS)
 
-AlgoTuner.addIntParam(cmd, "d_ran_routes", 1, 3)
-AlgoTuner.addFloatParam(cmd, "d_ratio", 0.005, 0.02)
+AlgoTuner.addIntParam(cmd, "d_ran_routes", 1, 4)
+AlgoTuner.addFloatParam(cmd, "d_ratio", 0.003, 0.02)
 AlgoTuner.addIntParam(cmd, "d_knn", 10, 40)
 AlgoTuner.addIntParam(cmd, "d_exp_routes", 1, 5)
 
 
 
-AlgoTuner.addInitialValues(cmd, [3, 0.02, 10, 5])
-AlgoTuner.tune(cmd, benchMark, 600, 4, [1023,1234,1244,1221], AlgoTuner.ShowAll)
+AlgoTuner.addInitialValues(cmd, [2, 0.01, 12, 2])
+AlgoTuner.tune(cmd, benchMark, 1000, 3, [1234,1244,1221], AlgoTuner.ShowAll)
--- src/Visualisation.jl
+++ src/Visualisation.jl
@@ -17,7 +17,6 @@ function draw_customers(s, coord)
 
     scale = 20
     for c in coord
-        # sethue("skyblue")
         sethue("black")
 
         circle((c[1]-70)*scale,(c[2]-70)*scale,15,:fill)
@@ -26,9 +25,6 @@ function draw_customers(s, coord)
     for i in 1:length(s)
         # randomcolor()
         sethue("grey")
-
-        # k = rand(1:length(s))
-        # sethue(cols[k][1])
         for j in 1:length(s[i])-1
             p1 = s[i][j]
             p2 = s[i][j+1]
@@ -50,62 +46,3 @@ function draw_customers(s, coord)
 end
 
 draw_customers(s, coord)
-# function draw(s)
-#
-#     Drawing(400,400)
-#     background("white")
-#
-#     #The point (0,0) is in the top left corner
-#     #this fuction moves it to the center of the canvas
-#     origin()
-#
-#     #Before drawing make sure to set the color to use
-#     sethue("red")
-#     #A rectangle can be drawn pasing the left top corner corrdinate
-#     #plus the width and height. The last parameters means drawing the outline
-#     #if you want a filled rectange use :fill
-#     rect(0,0,30,40,:stroke)
-#
-#     sethue("blue")
-#     #a circle can be drawn passing the center coordinate and the radius
-#     #this time we decided to fill the circle useing :fill
-#     circle(-50,10,5,:fill)
-#
-#     sethue("black")
-#     #Lines need to have two points passed as Point data structures
-#     line(Point(-80,0),Point(80,30),:stroke)
-#
-#     #Writes the giving line of text starting from the given point
-#     text("Hello drawing",Point(100,100))
-#
-#     #terminates and commits the drawing
-#     finish()
-#
-#     #makes a preview in e.g. Atom plot pannel
-#     preview()
-# end
-#
-# draw(1)
-# for d in coord
-#     draw_customers(d)
-# end
-# preview()
-# println(size(coord))
-
-
-# Graph of weight changes
-# remove_random_route = zeros(Float32, l)
-# remove_kexpensive_route = zeros(Float32, l)
-# remove_knn = zeros(Float32, l)
-# remove_ran_cust = zeros(Float32, l)
-#
-# for i in 1:l
-#     # println(y[i])
-#     remove_random_route[i] = y[i][1]
-#     remove_kexpensive_route[i] = y[i][2]
-#     remove_knn[i] = y[i][3]
-#     remove_knn[i] = y[i][4]
-# end
-# x = 1:l
-# println("here")
-# plot(1:l, [remove_random_route, remove_kexpensive_route, remove_knn, remove_ran_cust])
